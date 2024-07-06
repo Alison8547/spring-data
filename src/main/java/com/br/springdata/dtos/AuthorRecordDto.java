30715f954cadff23ace6d1eeb909fb55919e307f
Alison8547 <alisonailson100@hotmail.com>
2024-07-05 22:21:44 -0300

    feat: create and find author

---
+++ src/main/java/com/br/springdata/dtos/AuthorRecordDto.java
@@ -0,0 +1,4 @@
+package com.br.springdata.dtos;
+
+public record AuthorRecordDto(String name) {
+}
---
+++ src/main/java/com/br/springdata/service/AuthorService.java
@@ -0,0 +1,33 @@
+package com.br.springdata.service;
+
+import com.br.springdata.dtos.AuthorRecordDto;
+import com.br.springdata.exception.BusinessException;
+import com.br.springdata.models.AuthorModel;
+import com.br.springdata.repository.AuthorRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Transactional
+    public AuthorModel createAuthor(AuthorRecordDto authorRecordDto) {
+        AuthorModel author = new AuthorModel();
+        author.setName(authorRecordDto.name());
+
+        authorRepository.save(author);
+
+        return author;
+    }
+
+    public AuthorModel findAuthorById(UUID id) {
+        return authorRepository.findById(id).orElseThrow(() -> new BusinessException("No author found with id: " + id));
+    }
+
+}
